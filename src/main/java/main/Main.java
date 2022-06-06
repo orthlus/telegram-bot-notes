d479364093352d3cd2b637ba82461e0c166146d8
Шапран Константин <konstantin.shapran@domrf.ru>
2022-06-06 15:18:12 +0300

    clear

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -1,10 +1,8 @@
 package main;
 
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@Slf4j
 @SpringBootApplication
 public class Main {
 	public static void main(String[] args) {
