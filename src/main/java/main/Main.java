25a492e69dfcd8f8ef4e96df57b15ffca133412d
Шапран Константин <konstantin.shapran@domrf.ru>
2022-06-06 15:19:11 +0300

    TODO

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -9,6 +9,7 @@ public class Main {
 		SpringApplication.run(Main.class);
 		/*TODO
 		*  удалять с quartz отправленные файлы (класс DeleteFilesJob)
+		*  а может удалять файлы сразу после отправки? они больше не используются
 		*  добавить пдф файлы
 		*  добавить проверку наличия записей перед выгрузкой с помощью DbService.existsHistory
 		*  добавить функции для админа (ADMIN_TELEGRAM_ID) - статистику использования бота
